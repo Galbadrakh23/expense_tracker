adc10c325d27adc127db97383a235c5bb89eebb6
galbadrakh <galbadrak223@gmail.com>
2024-09-05 09:25:40 +0800

    dashboard

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -7,43 +7,43 @@ import { apiUrl } from "../../utils/util";
 import { toast } from "react-toastify";
 
 const Dashboard = () => {
-	const { user } = useContext(UserContext);
-	const [transactionData, setTransactionData] = useState([]);
+  const { user } = useContext(UserContext);
+  const [transactionData, setTransactionData] = useState([]);
 
-	const fetchTransactions = async () => {
-		try {
-			const res = await axios.get(`${apiUrl}/records/${user.id}`);
-			setTransactionData(res.data);
-		} catch (error) {
-			console.error(error);
-			toast.error("Failed to fetch transactions");
-		}
-	};
+  const fetchTransactions = async () => {
+    try {
+      const res = await axios.get(`${apiUrl}/records/${user.id}`);
+      setTransactionData(res.data);
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to fetch transactions");
+    }
+  };
 
-	useEffect(() => {
-		if (user && user.id) {
-			fetchTransactions();
-		}
-	}, [user.id]);
+  useEffect(() => {
+    if (user && user.id) {
+      fetchTransactions();
+    }
+  }, [user.id]);
 
-	return (
-		<div>
-			<div>
-				<h2>Records</h2>
-				{transactionData?.transactions?.map((transaction, index) => {
-					return (
-						<div key={index} className="flex">
-							<img src="/income.svg" alt="income" />
-							<div>
-								<p className="mb-1">{transaction?.name}</p>
-								<p className="text-[#6B7280]">{transaction?.createdat}</p>
-							</div>
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        <h2 className="text-2xl ml-2 mb-4">Records</h2>
+        {transactionData?.transactions?.map((transaction, index) => {
+          return (
+            <div key={index} className="flex">
+              <img src="/income.svg" alt="income" />
+              <div>
+                <p className="mb-1">{transaction?.name}</p>
+                <p className="text-[#6B7280]">{transaction?.createdat}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
